76a9492ed8d46df6539cff42d2f37ecfec3fdb93
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-11-08 20:03:01 -0300

    Uso de componentes TextInput e TouchableOpacity

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,29 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>React Native is ON!!!</Text>
+      <Text style={styles.text}>
+        Olá Leonardo
+      </Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder='Digite sua tarefa...'
+        placeholderTextColor='#555555'
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>
+          Adicionar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+// TextInput para entrada de dados
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
@@ -9,5 +9,27 @@ export const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     color: 'white',
+    margin: 20,
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: 'white',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginHorizontal: 20
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 10,
+    margin: 20,
+    alignItems: 'center',
+    borderRadius: 8  
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
+
+// Platform.OS permite testar qual SO está em uso, para realizar configurações específicas de ambiente.
